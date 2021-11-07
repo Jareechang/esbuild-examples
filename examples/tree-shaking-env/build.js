a703da474844cf7f06c599c88a1c165251cb5642
Jerry Chang <therealjerrycan@gmail.com>
2021-11-07 00:56:22 -0700

    update build

--- examples/tree-shaking-env/build.js
+++ examples/tree-shaking-env/build.js
@@ -1,15 +1,11 @@
 const esbuild = require('esbuild');
 
-async function main() {
-  esbuild.build({
-    entryPoints: ['./src/index.ts'],
-    bundle: true,
-    define: {
-      'process.env.NODE_ENV': '"production"'
-    },
-    outfile: './dist/index.js',
-    tsconfig: './tsconfig.json',
-  });
-}
-
-main();
+esbuild.build({
+  entryPoints: ['./src/index.ts'],
+  bundle: true,
+  define: {
+    'process.env.NODE_ENV': '"production"'
+  },
+  outfile: './dist/index.js',
+  tsconfig: './tsconfig.json',
+});
